1389f372a0bdd614e0ce3edd9f8af27d93de098d
wang.lihai <wang.lihai@huadongmedia.com>
2019-03-27 09:31:32 +0800

    这是一个测试

--- Charge/app/src/main/java/com/znxk/charge/ui/activity/TestActivity.java
+++ Charge/app/src/main/java/com/znxk/charge/ui/activity/TestActivity.java
@@ -10,6 +10,18 @@ public class TestActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
+
+
+
+
+
+
+
+
+
+        
         setContentView(R.layout.activity_test);
         System.out.println();
     }
